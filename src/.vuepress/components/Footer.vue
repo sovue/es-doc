6dfa8f4de6b52d789c1da7023d9e733f48f19066
Yegor Gunko <57867800+yegorgunko@users.noreply.github.com>
2021-06-08 13:24:39 +0300

    Исправлена ссылка на хостинг в футере

--- src/.vuepress/components/Footer.vue
+++ src/.vuepress/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="page-footer">
-    <a href="https://vercel.com?utm_source=sovue&utm_campaign=oss">
+    <a href="https://vercel.com/?utm_source=sovue&utm_campaign=oss">
       <img
         src="https://www.datocms-assets.com/31049/1618983297-powered-by-vercel.svg"
       />
