fab36cfa7007991e2a033efb196f4bc1841ae85e
alyoshaslab <139561770+ohfkgsofsex@users.noreply.github.com>
2026-05-26 07:02:36 +0300

    chore: Changed "pages" directory name to "routes"

--- app/app.py
+++ app/app.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
 from starlette.exceptions import HTTPException
 
-from .pages import main_router
+from .routes import main_router
 
 app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
 app.include_router(main_router)
