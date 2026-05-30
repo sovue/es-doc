01165f9ce61141be69250c0a358aef78fb0e5e83
alyoshaslab <139561770+ohfkgsofsex@users.noreply.github.com>
2026-05-30 04:47:22 +0300

    feat: HTTP error page prototype

--- app/app.py
+++ app/app.py
@@ -1,12 +1,23 @@
-from fastapi import FastAPI
-from fastapi.responses import PlainTextResponse
+from fastapi import FastAPI, Request
 from starlette.exceptions import HTTPException
 
 from .routes import main_router
+from .utils.file import templates
 
 app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
 app.include_router(main_router)
 
 @app.exception_handler(HTTPException)
-async def http_exception_handler(request, exc: HTTPException):
-    return PlainTextResponse(str(exc.detail), status_code=exc.status_code)
+async def http_exception_handler(request: Request, exc: HTTPException):
+    details = {
+        404: (
+            'Страница не найдена.',
+            'Проверьте адрес страницы или вернитесь на главную.'
+        ),
+    }
+    detail, description = details.get(exc.status_code, (exc.detail, 'Во время загрузки страницы произошла ошибка. Попробуйте повторить действие позже. Если проблема сохраняется — сообщите администрации.'))
+    return templates.TemplateResponse(request, 'error.html', {
+        'errno': exc.status_code,
+        'detail': detail,
+        'description': description
+    }, status_code=exc.status_code)
